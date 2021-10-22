35f160d63970dca1cc14ecd144a0542f36d12b3d
Victor Mal <VictorianusVelikyu@gmail.com>
2021-10-23 01:53:01 +0300

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  cardNumber: "5312122312331213",
+  cvv: 123,
+  dbPass: "password",
+
+  THEMES: {
+    LIGHT: "LIGHT-THEME",
+    DARK: "DARK-THEME",
+    YELLOW: "YELLOW-THEME",
+  },
+};
+
+export default constants;
